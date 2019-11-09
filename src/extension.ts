941caaba3c90ba8d58a9573f98a0aaac5bdb2a5e
Mehran Ganjgahi <M.ganjgahi@haj.ir>
2019-11-09 16:59:15 +0330

    webview

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import startTimer, {setRemoteToken} from './lib/startTimer';
+import startTimer, { setRemoteToken } from './lib/startTimer';
 import statusBarHandler from './lib/statusBar';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -21,21 +21,59 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello World!');
 	});
 
-	let setUserConfig = vscode.commands.registerCommand("extansion.setUserCongif", ()=>{
-		vscode.window.showInputBox({placeHolder: 'set your remote id'}).then((result) => {
-			if(result){
+	let setUserConfig = vscode.commands.registerCommand("extansion.setUserCongif", () => {
+		vscode.window.showInputBox({ placeHolder: 'set your remote id' }).then((result) => {
+			if (result) {
 				setRemoteToken(result)
 				vscode.workspace.getConfiguration("remoteId").update("static.conf.nili.RemoteId", result)
 			}
 
 		})
-	})
-
+	});
+	let webViewStart = vscode.commands.registerCommand('extansion.webViewStart', () => {
+		// Create and show a new webview
+		const panel = vscode.window.createWebviewPanel(
+			'DevTime', // Identifies the type of the webview. Used internally
+			'Dev Time', // Title of the panel displayed to the user
+			vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+			{} // Webview options. More on these later.
+		);
+		panel.webview.html = getWebviewContent();
+	});
 	context.subscriptions.push(disposable);
 	context.subscriptions.push(setUserConfig);
+	context.subscriptions.push(webViewStart);
 	statusBarHandler(context);
 	startTimer();
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+
+export function deactivate() { }
+function getWebviewContent() {
+	return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <meta http-equiv="Content-Security-Policy" content="default-src 'none';">
+	  <title>DEV</title>
+  </head>
+  <body>
+
+		<h1 class="header">web View Content</h1>
+		<h1 id="lines-of-code-counter">0</h1>
+		
+		<script>
+		(function() {
+            const vscode = acquireVsCodeApi();
+            const counter = document.getElementById('lines-of-code-counter');
+
+            let count = 0;
+            setInterval(() => {
+                counter.textContent = count++;
+            }, 100);
+        }())
+		</script>
+  </body>
+  </html>`;
+}
